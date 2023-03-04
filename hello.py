e8d919f43e2613b2b6f1312718b3a9fc638907b3
bochaoli <bochaoli97@gmail.com>
2023-03-04 11:12:41 -0700

    add 4th line from origin

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ print("In branch bochao, Hello world!")
 print("Add some changes in bochao branch from remote in conflix in remote")
 
 print("added a third line from remote")
+
+print("add 4th line from local")
